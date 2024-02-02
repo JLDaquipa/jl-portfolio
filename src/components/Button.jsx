913b7cc7a966ecc5886127af64ec6a3a0027df3f
Jay Lloyd Daquipa <31461040+JLDaquipa@users.noreply.github.com>
2024-02-02 11:40:17 +0800

    Improved form validation on contact and added loading state spinner and notification for improve ux

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 
-const Button = ({children, className}) => {
+const Button = ({children, className, disabled = false}) => {
   return (
-    <button className={`w-fit linear-bg py-3 px-4 font-semibold text-black rounded-xl ${className}`}>
+    <button className={`w-fit linear-bg py-3 px-4 font-semibold text-black rounded-xl ${className}`} disabled={disabled}>
       {children}
     </button>
   )
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,12 +1,14 @@
 import { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
 import Button from "../components/Button";
+import { ring2 } from 'ldrs'
 
 const Contact = () => {
   const form = useRef();
   const [onSend,setOnSend] = useState(false);
   const [sent, setSent] = useState(false);
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(false);
+  ring2.register()
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -16,9 +18,11 @@ const Contact = () => {
     emailjs.sendForm('service_v8zpsca', 'template_3jdy69b', form.current, 'SshgGdjNiriKxXE3u')
       .then((result) => {
           e.target.reset();
+          setOnSend(false)
           setSent(true)
           console.log(result.text);
       }, (error) => {
+          setOnSend(false)
           setError(true)
           console.log(error.text);
       });
@@ -112,12 +116,26 @@ const Contact = () => {
                 />
               </div>
               <div className="flex items-center gap-4">
-                <Button className="flex gap-2">
-                  Send Message <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14.2199 21.9354C13.0399 21.9354 11.3699 21.1054 10.0499 17.1354L9.32988 14.9754L7.16988 14.2554C3.20988 12.9354 2.37988 11.2654 2.37988 10.0854C2.37988 8.91549 3.20988 7.23549 7.16988 5.90549L15.6599 3.07549C17.7799 2.36549 19.5499 2.57549 20.6399 3.65549C21.7299 4.73549 21.9399 6.51549 21.2299 8.63549L18.3999 17.1254C17.0699 21.1054 15.3999 21.9354 14.2199 21.9354ZM7.63988 7.33549C4.85988 8.26549 3.86988 9.36549 3.86988 10.0854C3.86988 10.8054 4.85988 11.9054 7.63988 12.8254L10.1599 13.6654C10.3799 13.7354 10.5599 13.9154 10.6299 14.1354L11.4699 16.6554C12.3899 19.4354 13.4999 20.4254 14.2199 20.4254C14.9399 20.4254 16.0399 19.4354 16.9699 16.6554L19.7999 8.16549C20.3099 6.62549 20.2199 5.36549 19.5699 4.71549C18.9199 4.06549 17.6599 3.98549 16.1299 4.49549L7.63988 7.33549Z" fill="black"/>
-                  <path d="M10.11 14.7052C9.92005 14.7052 9.73005 14.6352 9.58005 14.4852C9.29005 14.1952 9.29005 13.7152 9.58005 13.4252L13.16 9.83518C13.45 9.54518 13.93 9.54518 14.22 9.83518C14.51 10.1252 14.51 10.6052 14.22 10.8952L10.64 14.4852C10.5 14.6352 10.3 14.7052 10.11 14.7052Z" fill="black"/>
-                  </svg>
-                </Button>
+                  <Button className="flex justify-center items-center gap-2 w-[183px] h-[48px]" disabled={onSend}>
+                    { onSend ? 
+                      <l-ring-2
+                        size="25"
+                        stroke="2"
+                        stroke-length="0.25"
+                        bg-opacity="0.1"
+                        speed="0.8" 
+                        color="black" 
+                      ></l-ring-2> 
+                      : 
+                      <>
+                        <span>Send Message</span> <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14.2199 21.9354C13.0399 21.9354 11.3699 21.1054 10.0499 17.1354L9.32988 14.9754L7.16988 14.2554C3.20988 12.9354 2.37988 11.2654 2.37988 10.0854C2.37988 8.91549 3.20988 7.23549 7.16988 5.90549L15.6599 3.07549C17.7799 2.36549 19.5499 2.57549 20.6399 3.65549C21.7299 4.73549 21.9399 6.51549 21.2299 8.63549L18.3999 17.1254C17.0699 21.1054 15.3999 21.9354 14.2199 21.9354ZM7.63988 7.33549C4.85988 8.26549 3.86988 9.36549 3.86988 10.0854C3.86988 10.8054 4.85988 11.9054 7.63988 12.8254L10.1599 13.6654C10.3799 13.7354 10.5599 13.9154 10.6299 14.1354L11.4699 16.6554C12.3899 19.4354 13.4999 20.4254 14.2199 20.4254C14.9399 20.4254 16.0399 19.4354 16.9699 16.6554L19.7999 8.16549C20.3099 6.62549 20.2199 5.36549 19.5699 4.71549C18.9199 4.06549 17.6599 3.98549 16.1299 4.49549L7.63988 7.33549Z" fill="black"/>
+                        <path d="M10.11 14.7052C9.92005 14.7052 9.73005 14.6352 9.58005 14.4852C9.29005 14.1952 9.29005 13.7152 9.58005 13.4252L13.16 9.83518C13.45 9.54518 13.93 9.54518 14.22 9.83518C14.51 10.1252 14.51 10.6052 14.22 10.8952L10.64 14.4852C10.5 14.6352 10.3 14.7052 10.11 14.7052Z" fill="black"/>
+                        </svg>
+                      </>
+                    }
+                  </Button>
+                
                 {sent && <p className="font-semibold text-green-500">Message sent!</p>}
                 {error && <p className="font-semibold text-red-500">Message not sent!</p>}
               </div>
